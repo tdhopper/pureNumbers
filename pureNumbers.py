e1d3814de1a44138e7e537da4d73db37ff721e3e
Kevin Sheth <kevinsheth25@gmail.com>
2018-10-26 10:47:19 -0400

    changed to just nic, starting there

--- pureNumbers.py
+++ pureNumbers.py
@@ -1,5 +1,5 @@
 
-
+import math
 
 
 # pureNumbers calculates the amount of green coffee and each batch size necessary for
@@ -7,56 +7,38 @@
 
 #Variables
 
-# tr = total roast (ex. 500 pounds of costa rica are needed)
-# nic = org nic nortena
-# costa = org costa rica lig
-# col = org colombia la prima
-# green = green coffee
-# brown = roasted coffee
-# b = batch size (ex. the roaster can roast 62 lbs at a time)
-# n = number of roasts
-# r = remainder
-# lb = last batch
 
 # pure coffee recipe
 
 # 60% org nic nortena
-# 15% org costa rica lig
-# 25% org colombia la prima
 
 # green to roasted coffee yields
 
 # nic = 0.83870968
-# costa = 0.8483871
-# col = 0.8483871 *arbitrary*
 
 #determine roasted coffee individual
 
-nic = 500 * 0.6
-costa = 500 * 0.15
-col = 500 * 0.25
+total_coffee_needed = 500
+nic_needed = total_coffee_needed * 0.6
 
 #determine green coffee individual
 
-nic = nic / 0.83870968
-costa = costa / 0.8483871
-col = col / 0.8483871
+greennic = nic_needed / 0.83870968
 
 #declare last batch
 
-lb = 0
+
 
 while(lb < 45):
     b = 62
-    nic = 500 * 0.6
     nic = nic / 0.83870968
-    tr = nic
+    tr = math.ceil(nic)
     n = tr / b
     x = [x for x in range(10000) if n - x > 0][-1]
     r = n - x
     lb = r * b
     n = x + 1
-    b - 1
+
+
 print("You need to do " + str(n) + " batches total.")
-print(b)
-print(lb)
+print("You should do " +  str(n-1) + " batches of " + str(b) + " and 1 batch of " + str(lb))
